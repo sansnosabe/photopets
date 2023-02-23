6bc8644118ee59c5dee0564d73d68a277646b012
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-02-24 00:28:01 +0100

    add uploads gitignore

--- db/queries/posts/selectPostsQuery.js
+++ db/queries/posts/selectPostsQuery.js
@@ -1,4 +1,4 @@
-const getDB = require('../../getDB');
+const getDB = require("../../getDB");
 
 const selectPostsQuery = async (id_user, keyword = "") => {
   let connection;
@@ -8,19 +8,19 @@ const selectPostsQuery = async (id_user, keyword = "") => {
 
     const [posts] = await connection.query(
       `
-                SELECT 
-                    P.*,
-                    COUNT(L.id) AS likes,
-                    BIT_OR(L.id_user = ?) AS myLikes,
-                    U.name AS user,
-                    IFNULL(P.id_user = ?, 0) AS owner
-                FROM posts P
-                INNER JOIN users U ON U.id = P.id_user
-                LEFT JOIN likes L ON L.id_post = P.id
-                WHERE P.text LIKE ?
-                GROUP BY P.id
-                ORDER BY P.created_at DESC
-            `,
+      SELECT 
+        P.*,
+        COUNT(L.id) AS likes,
+        BIT_OR(L.id_user = ?) AS myLikes,
+        U.name AS user,
+        IFNULL(P.id_user = ?, 0) AS owner
+      FROM posts P
+      INNER JOIN users U ON U.id = P.id_user
+      LEFT JOIN likes L ON L.id_post = P.id
+      WHERE P.text LIKE ?
+      GROUP BY P.id
+    ORDER BY P.created_at DESC
+      `,
       [id_user, id_user, `%${keyword}%`]
     );
 
