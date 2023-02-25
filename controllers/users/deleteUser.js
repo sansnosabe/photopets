342e422273eb1ad19232ad8c762da11b6eb28236
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-02-25 16:00:30 +0100

    add space

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -2,6 +2,7 @@ const selectUserByIdQuery = require("../../db/queries/users/selectUserByIdQuery"
 const selectPostsByUserIdQuery = require("../../db/queries/posts/selectPostsByUserIdQuery");
 const deleteUserQuery = require("../../db/queries/users/deleteUserQuery");
 const { deleteImg, generateError } = require("../../helpers");
+
 const deleteUser = async (req, res, next) => {
   try {
     const user = await selectUserByIdQuery(req.user.id);
