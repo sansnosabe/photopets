1ee395949a51b00a925b5af0484a340a6b262982
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-02-25 01:44:48 +0100

    fix error delete images

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -1,17 +1,28 @@
 const selectUserByIdQuery = require("../../db/queries/users/selectUserByIdQuery");
 const selectPostsByUserIdQuery = require("../../db/queries/posts/selectPostsByUserIdQuery");
 const deleteUserQuery = require("../../db/queries/users/deleteUserQuery");
-const { deleteImg } = require("../../helpers");
+const { deleteImg, generateError } = require("../../helpers");
 
 const deleteUser = async (req, res, next) => {
   try {
     const user = await selectUserByIdQuery(req.user.id);
     const posts = await selectPostsByUserIdQuery(req.user.id);
 
-    if (user.avatar && posts[0].image) {
+    if (user.avatar) {
       await deleteImg(user.avatar);
-      await deleteImg(posts[0].image);
     }
+
+    if (posts instanceof Error) {
+      throw posts;
+    }
+
+    await Promise.all(
+      posts.map(async (post) => {
+        if (post.image) {
+          await deleteImg(post.image);
+        }
+      })
+    );
 
     await deleteUserQuery(req.user.id);
 
@@ -21,7 +32,11 @@ const deleteUser = async (req, res, next) => {
       message: "Usuario eliminado",
     });
   } catch (err) {
-    next(err);
+    if (err instanceof Error) {
+      return next(err);
+    }
+    const error = generateError("Error al eliminar usuario", 500);
+    return next(error);
   }
 };
 
--- db/queries/posts/selectPostsByUserIdQuery.js
+++ db/queries/posts/selectPostsByUserIdQuery.js
@@ -9,8 +9,8 @@ const selectPostsByUserIdQuery = async (idUser) => {
     connection = await getDB();
     const [userPosts] = await connection.query(`SELECT id, text, image FROM posts WHERE id_user = ?`, [idUser]);
 
-    if (userPosts.length < 1) {
-      return generateError("Usuario no tiene posts", 404);
+    if (userPosts.length < 0) {
+      return generateError("El Usuario no tiene ningun post", 404);
     }
 
     return userPosts;
