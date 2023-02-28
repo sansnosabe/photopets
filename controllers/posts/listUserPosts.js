955b6a31707bd0a2fde44ba2f420fa7c673aecca
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-02-28 04:22:23 +0100

    add error messages

--- controllers/posts/listUserPosts.js
+++ controllers/posts/listUserPosts.js
@@ -1,10 +1,14 @@
 const selectPostsByUserIdQuery = require("../../db/queries/posts/selectPostsByUserIdQuery");
+const { generateError } = require("../../helpers");
 
 const listUserPosts = async (req, res, next) => {
   try {
     const idUser = req.params.idUser;
     const posts = await selectPostsByUserIdQuery(idUser);
 
+    if (posts.length === 0) {
+      generateError("No existe ningun post con este ID");
+    }
     res.send({
       code: 200,
       status: "ok",
