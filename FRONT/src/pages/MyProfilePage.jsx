5f185bd5a513350bad7121bf53841ed9e9179d1e
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-04-27 06:25:14 +0200

    delete link

--- FRONT/src/pages/MyProfilePage.jsx
+++ FRONT/src/pages/MyProfilePage.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
 import { usePosts } from "../hooks/usePosts";
 import { useUsers } from "../hooks/useUsers";
 
@@ -23,13 +22,15 @@ export const MyProfilePage = () => {
 				<div>
 					<NewPost updateUserPosts={updateUserPosts} />
 					<header className="flex justify-center items-center space-x-10 mb-5">
-						<figure className="">
-							<img className="h-52 w-52 rounded-xl object-cover transform" src={`${import.meta.env.VITE_BACKEND}/public/${user.avatar}`} alt={user.username} />
+						<figure>
+							<img
+								className="h-40 w-40 rounded-full object-cover transform"
+								src={`${import.meta.env.VITE_BACKEND}/public/${user.avatar}`}
+								alt={user.username}
+							/>
 						</figure>
-						<main className="w-[100%]">
-							<div className="flex justify-between">
-								<h1 className="text-2xl text-left pl-1 font-bold mt-4">{user.username}</h1>
-							</div>
+						<main>
+							<h1 className="text-2xl text-left pl-1 font-bold mt-4">{user.username}</h1>
 							<div className="flex mt-2">
 								<span className="bg-gray-200 text-gray-700 rounded-full px-2 py-1 text-xs font-bold mr-2">{user.breed}</span>
 								<span className="bg-gray-200 text-gray-700 rounded-full px-2 py-1 text-xs font-bold">{user.kind}</span>
