b65753e81441a05fb255c92d0d0f6b38ac22bdd2
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-04-27 15:12:55 +0200

    prettier

--- FRONT/src/pages/EditProfilePage.jsx
+++ FRONT/src/pages/EditProfilePage.jsx
@@ -2,15 +2,14 @@ import { useState, useEffect } from "react";
 import { EditProfile } from "../components/EditProfile";
 
 export const EditProfilePage = () => {
-  const [update, setUpdate] = useState(false);
-  const forceUpdate = () => setUpdate(!update);
+	const [update, setUpdate] = useState(false);
+	const forceUpdate = () => setUpdate(!update);
 
-	useEffect(() => {
-  }, [update]);
+	useEffect(() => {}, [update]);
 
-  return (
-    <article>
-      <EditProfile forceUpdate={forceUpdate} />
-    </article>
-  );
+	return (
+		<article>
+			<EditProfile forceUpdate={forceUpdate} />
+		</article>
+	);
 };
